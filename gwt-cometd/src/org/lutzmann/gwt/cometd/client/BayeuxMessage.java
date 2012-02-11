0deeab8da3556e3a761313b0c612332b3fed9c7b
Bernhard Lutzmann <bernhard.lutzmann@gmail.com>
2012-02-11 23:36:01 +0100

    Initial commit

---
+++ gwt-cometd/src/org/lutzmann/gwt/cometd/client/BayeuxMessage.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.lutzmann.gwt.cometd.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class BayeuxMessage extends JavaScriptObject {
+
+	protected BayeuxMessage() {
+	}
+
+	public final native String getChannel() /*-{
+		return this.channel;
+	}-*/;
+
+	public final native String getVersion() /*-{
+		return this.version;
+	}-*/;
+
+	public final native String getMinimumVersion() /*-{
+		return this.minimumVersion;
+	}-*/;
+
+	public final native String getSupportedConnectionTypes() /*-{
+		return this.supportedConnectionTypes;
+	}-*/;
+
+	public final native String getClientId() /*-{
+		return this.clientId;
+	}-*/;
+
+	public final native String getConnectionType() /*-{
+		return this.connectionType;
+	}-*/;
+
+	public final native String getId() /*-{
+		return this.id;
+	}-*/;
+
+	public final native String getTimestamp() /*-{
+		return this.timestamp;
+	}-*/;
+
+	public final native JavaScriptObject getData() /*-{
+		return this.data;
+	}-*/;
+
+	public final native boolean isSuccessful() /*-{
+		return this.successful;
+	}-*/;
+
+	public final native String getSubscription() /*-{
+		return this.subscription;
+	}-*/;
+
+	public final native String getError() /*-{
+		return this.error;
+	}-*/;
+
+	public final native String getExt() /*-{
+		return this.ext;
+	}-*/;
+}
---
+++ gwt-cometd/src/org/lutzmann/gwt/cometd/client/CometD.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.lutzmann.gwt.cometd.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class CometD {
+
+	private static final String META_PREFIX = "/meta/";
+
+	private String cometURL;
+
+	public CometD(String url) {
+		this.cometURL = url;
+
+		addUnload();
+	}
+
+	private native void addUnload() /*-{
+		$wnd.dojo.addOnLoad(function() {
+			$wnd.dojo.addOnUnload(function() {
+				$wnd.dojox.cometd.disconnect(true);
+			});
+		});
+	}-*/;
+
+	public native void disableWebSocketTransport() /*-{
+		$wnd.dojox.cometd.websocketEnabled = false;
+	}-*/;
+
+	public native void configure() /*-{
+		var cometURL = this.@org.lutzmann.gwt.cometd.client.CometD::cometURL;
+
+		$wnd.dojox.cometd.configure({
+			url : cometURL,
+			logLevel : 'debug'
+		});
+	}-*/;
+
+	public native void handshake() /*-{
+		$wnd.dojox.cometd.handshake();
+	}-*/;
+
+	public native void disconnect() /*-{
+		$wnd.dojox.cometd.disconnect(true);
+	}-*/;
+
+	public native boolean isDisconnected() /*-{
+		return $wnd.dojox.cometd.isDisconnected();
+	}-*/;
+
+	public native JavaScriptObject addListener(String channel,
+			CometDListener<? extends JavaScriptObject> listener) /*-{
+		return $wnd.dojox.cometd
+				.addListener(
+						channel,
+						function(message) {
+							listener.@org.lutzmann.gwt.cometd.client.CometDListener::processMessage(Lcom/google/gwt/core/client/JavaScriptObject;)(message);
+						});
+	}-*/;
+
+	public JavaScriptObject addMetaListener(MetaChannel metaChannel,
+			CometDListener<? extends JavaScriptObject> listener) {
+		return addListener(META_PREFIX + metaChannel.toString(), listener);
+	}
+
+	public native void removeListener(
+			CometDListener<? extends JavaScriptObject> listener) /*-{
+		$wnd.dojox.cometd.removeListener(listener);
+	}-*/;
+
+	public native JavaScriptObject subscribe(String channel,
+			CometDListener<? extends JavaScriptObject> listener) /*-{
+		return $wnd.dojox.cometd
+				.subscribe(
+						channel,
+						function(message) {
+							listener.@org.lutzmann.gwt.cometd.client.CometDListener::processMessage(Lcom/google/gwt/core/client/JavaScriptObject;)(message);
+						});
+	}-*/;
+
+	public native void unsubscribe(JavaScriptObject subscription) /*-{
+		$wnd.dojox.cometd.unsubscribe(subscription);
+	}-*/;
+
+	public native void publish(String channel, JavaScriptObject jso) /*-{
+		$wnd.dojox.cometd.publish(channel, jso);
+	}-*/;
+
+	public native void startBatch() /*-{
+		$wnd.dojox.cometd.startBatch();
+	}-*/;
+
+	public native void endBatch() /*-{
+		$wnd.dojox.cometd.endBatch();
+	}-*/;
+}
